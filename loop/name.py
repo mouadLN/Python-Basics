cb4982bc810859da1a14f7f6f9f4d4102d66df45
mouadLN <lajanemouad@gmail.com>
2024-12-04 01:21:45 +0100

    update

--- loop/name.py
+++ loop/name.py
@@ -1,5 +1,5 @@
 
 for name in range (0,1):
     name = "LAJANE MOUAD"
-    print(name , end=' (')
+    print(name , end='(')
     print(len (name) , end=')')
---
+++ python-fund/Tr2.py
@@ -0,0 +1,5 @@
+x=1
+y=50
+while x!=y:
+    x +=1    
+    print(x)
