7aec8e791103eba149032389305c1679e94920f4
mouadLN <lajanemouad@gmail.com>
2024-12-05 01:42:00 +0100

    Create try.py

---
+++ fonction/try.py
@@ -0,0 +1,48 @@
+def add(a,b):
+    return(a + b)
+
+def sous(a,b):
+    return(a - b)
+
+def div(a,b):
+    return(a / b)
+
+def div(a,b):
+    return(a / b)
+
+def mul(a,b):
+    return(a * b)
+
+def carr(a,b):
+    return(a ** b)
+
+
+
+while True :
+    a = int(input("enter the first number :"))
+    b = int(input("enter the second number :"))
+    opp = input("choose a operation ( + , - , * , / , **) ")
+
+    if opp == "+" :
+        print(add(a,b))
+        
+    if opp == "-" :
+        print(sous(a,b))
+        
+    if opp == "*" :
+        print(mul(a,b))
+        
+    if opp == "/" :
+        print(div(a,b))
+        
+        if b == 0 :
+            print("divising by 0 is not possible")
+    if opp == "**" :
+        print(carr(a,b))
+    
+        
+    
+    
+
+
+
